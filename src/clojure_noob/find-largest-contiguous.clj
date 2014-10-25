173899a3ffa0c7f76a9fb66b6065f08b2b6037a2
tonyvanriet <tonyvanriet@TvRsMBP.hsd1.il.comcast.net>
2014-10-25 00:54:43 -0500

    basic implementation of find-largest-contiguous

--- src/clojure_noob/find-largest-contiguous.clj
+++ src/clojure_noob/find-largest-contiguous.clj
@@ -9,14 +9,61 @@
 
 
 (defn find-largest-contiguous [nums]
-  (loop [nums nums]
+
+  (loop [nums nums
+         previous-stats {:max 0
+                         :count 0
+                         :start-index 0
+                         :best-max 0
+                         :best-count 0
+                         :best-start-index 0}]
+
     (let [current-num (first nums)
           rest-nums (rest nums)]
-      (println (str "num " current-num))
 
-      (if (not (empty? rest-nums))
-        (recur (rest-nums))))))
+      (println current-num previous-stats)
+
+      (if (nil? current-num)
+
+        previous-stats
+
+        (do
+
+          (def current-max (+ (:max previous-stats) current-num))
+          (def current-count (inc (:count previous-stats)))
+
+          (println "current-count " current-count)
+          (println "current index " (+ (:start-index previous-stats) current-count -1))
+
+          (if (<= current-max 0)
+
+            (recur rest-nums {:max 0
+                              :count 0
+                              :start-index (+ (:start-index previous-stats) current-count)
+                              :best-max (:best-max previous-stats)
+                              :best-count (:best-count previous-stats)
+                              :best-start-index (:best-start-index previous-stats)})
+
+            (if (> current-max (:best-max previous-stats))
+
+              (recur rest-nums {:max current-max
+                                :count current-count
+                                :start-index (:start-index previous-stats)
+                                :best-max current-max
+                                :best-count current-count
+                                :best-start-index (:start-index previous-stats)})
+
+              (recur rest-nums {:max current-max
+                                :count current-count
+                                :start-index (:start-index previous-stats)
+                                :best-max (:best-max previous-stats)
+                                :best-count (:best-count previous-stats)
+                                :best-start-index (:best-start-index previous-stats)}))))))))
 
 
-(find-largest-contiguous nums)
+
+
+
+ (find-largest-contiguous nums)
+
 
