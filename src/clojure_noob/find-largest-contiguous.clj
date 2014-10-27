5af94e4b2e71edb5a5b1d1d32cb088db3be907fd
tonyvanriet <tonyvanriet@TvRsMBP.hsd1.il.comcast.net>
2014-10-26 22:56:08 -0500

    refactor stats into separate previous and best maps
    recur calls look a lot better

--- src/clojure_noob/find-largest-contiguous.clj
+++ src/clojure_noob/find-largest-contiguous.clj
@@ -2,8 +2,10 @@
   (:gen-class))
 
 
+
 ; Find the largest contiguous array
 ; http://whostolebenfrog.github.io/interviews/2014/06/27/contiguous.html
+
 
 (def nums [-4 3 0 -1 -6 4 5 -2 3 -1 2 -5 4])
 
@@ -13,54 +15,45 @@
   (loop [nums nums
          previous-stats {:max 0
                          :count 0
-                         :start-index 0
-                         :best-max 0
-                         :best-count 0
-                         :best-start-index 0}]
+                         :start-index 0}
+         best-stats {:max 0
+                     :count 0
+                     :start-index 0}]
 
     (let [current-num (first nums)
           rest-nums (rest nums)]
 
-      (println current-num previous-stats)
-
       (if (nil? current-num)
 
-        previous-stats
+        ; when we're all out of nums, return the best results up through the recursion
+        best-stats
 
         (do
 
-          (def current-max (+ (:max previous-stats) current-num))
-          (def current-count (inc (:count previous-stats)))
+          (def current-stats {:max (+ (:max previous-stats) current-num)
+                              :count (inc (:count previous-stats))
+                              :start-index (:start-index previous-stats)})
 
-          (println "current-count " current-count)
-          (println "current index " (+ (:start-index previous-stats) current-count -1))
+          (println current-num)
+          (println current-stats)
 
-          (if (<= current-max 0)
+          (if (<= (:max current-stats) 0)
 
-            (recur rest-nums {:max 0
-                              :count 0
-                              :start-index (+ (:start-index previous-stats) current-count)
-                              :best-max (:best-max previous-stats)
-                              :best-count (:best-count previous-stats)
-                              :best-start-index (:best-start-index previous-stats)})
+            (recur rest-nums
+                   {:max 0
+                    :count 0
+                    :start-index (+ (:start-index current-stats) (:count current-stats))}
+                   best-stats)
 
-            (if (> current-max (:best-max previous-stats))
+              (if (> (:max current-stats) (:max best-stats))
 
-              (recur rest-nums {:max current-max
-                                :count current-count
-                                :start-index (:start-index previous-stats)
-                                :best-max current-max
-                                :best-count current-count
-                                :best-start-index (:start-index previous-stats)})
+                (recur rest-nums
+                       current-stats
+                       current-stats)
 
-              (recur rest-nums {:max current-max
-                                :count current-count
-                                :start-index (:start-index previous-stats)
-                                :best-max (:best-max previous-stats)
-                                :best-count (:best-count previous-stats)
-                                :best-start-index (:best-start-index previous-stats)}))))))))
-
-
+                (recur rest-nums
+                       current-stats
+                       best-stats))))))))
 
 
 
